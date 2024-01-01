13df37f776140abfdb7b7638b6a3c4422d05ac1e
Mike Tuntelder <mike.luchies@gmail.com>
2024-01-01 12:59:29 +0100

    loop script every 5 mins if value changed then update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const axios = require("axios");
 require("dotenv").config();
 
+console.log("# | Script started");
+
 const homeAssistantApiUrl = process.env.HOME_ASSISTANT_API_URL;
 const yagccApiUrl = process.env.YAGCC_API_URL;
 const weightSensorEntity = process.env.HOME_ASSISTANT_SENSOR;
+const pollingInterval = process.env.POLLING_INTERVAL || 300000; // Default: 5 minutes
+let lastUpdatedTimestamp = null;
 
 const fetchSensorData = async () => {
+  console.log("# | Running loop");
   try {
     const response = await axios.get(
       `${homeAssistantApiUrl}/api/states/${weightSensorEntity}`,
@@ -15,12 +20,21 @@ const fetchSensorData = async () => {
         },
       }
     );
-    console.log(process.env.GARMIN_EMAIL);
-    console.log(process.env.YAGCC_API_URL);
-    console.log(process.env.HOME_ASSISTANT_SENSOR);
-    console.log(process.env.weight);
-
     const weight = parseFloat(response.data.state);
+    const currentLastUpdated = new Date(response.data.last_updated).getTime();
+
+    // Check if the value has been updated since the last check
+    if (
+      lastUpdatedTimestamp !== null &&
+      currentLastUpdated <= lastUpdatedTimestamp
+    ) {
+      console.log(
+        "# | Sensor value has not been updated since the last check."
+      );
+      return;
+    }
+
+    lastUpdatedTimestamp = currentLastUpdated;
 
     // Construct the payload for the YAGCC API
     const yagccPayload = {
@@ -32,17 +46,27 @@ const fetchSensorData = async () => {
 
     // Send a POST request to the YAGCC API
     const yagccResponse = await axios.post(yagccApiUrl, yagccPayload);
-    if (yagccResponse.status == 201) {
+    if (yagccResponse.status === 201) {
       console.log(
-        `Successfully sent weight of ${weight} to the following garmin account: ${process.env.GARMIN_EMAIL}`
+        `# | Successfully sent weight of ${weight} to the following Garmin account: ${process.env.GARMIN_EMAIL}`
       );
     } else {
-      console.log("YAGCC Response Status:", yagccResponse.status); // Log the HTTP status code
-      console.log("YAGCC Response Data:", yagccResponse.data);
+      console.log("# | YAGCC Response Status:", yagccResponse.status); // Log the HTTP status code
+      console.log("# | YAGCC Response Data:", yagccResponse.data);
     }
   } catch (error) {
-    console.error("Error:", error.message);
+    console.error("# | Error:", error.message);
+  }
+
+  console.log("# | Closing loop");
+};
+
+const runScript = async () => {
+  while (true) {
+    await fetchSensorData();
+    console.log("# | Sleeping for 5 mins");
+    await new Promise((resolve) => setTimeout(resolve, pollingInterval));
   }
 };
 
-fetchSensorData();
+runScript();
